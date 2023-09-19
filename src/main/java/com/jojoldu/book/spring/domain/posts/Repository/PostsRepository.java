07d2f5e818a4105f110252b44c36e61d34fdf4d5
JoonGit <ljj9730@gmail.com>
2023-09-19 10:18:47 +0900

    Resource_Exchange_Controller_Test_End

--- src/main/java/com/jojoldu/book/spring/domain/posts/Repository/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jojoldu.book.spring.domain.posts.Repository;
-
-import com.jojoldu.book.spring.domain.posts.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
--- src/main/java/com/jojoldu/book/spring/domain/posts/Repository/Resource/ResourceInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jojoldu.book.spring.domain.posts.Repository.Resource;
-
-import com.jojoldu.book.spring.domain.posts.Entity.ResourceInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceInfoRepository extends JpaRepository<ResourceInfoEntity, Long> {
-
-}
